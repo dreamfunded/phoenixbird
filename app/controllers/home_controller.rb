4b86a1e8bd5a40a01637e89c7fd56e0bfab6f364
alexander <alexandr.larionov88@gmail.com>
2017-03-01 11:07:26 -0800

    homepage top 3 raising companies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	invisible_captcha only: [:liquidate_form, :contact_us_send_email, :get_funded_send]
 
 	def index
-		#@companies = Company.where.not(invested_amount: nil).order("invested_amount DESC").first(3)
+		@companies = Company.where.not(invested_amount: nil).order("invested_amount DESC").first(3)
 	end
 
 	def staging
