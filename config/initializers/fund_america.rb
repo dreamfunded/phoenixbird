e10b98e46f4f15b84ac7fa71a08a60641bf969be
alexander <alexandr.larionov88@gmail.com>
2017-03-20 16:32:29 -0700

    fix

--- config/initializers/fund_america.rb
+++ config/initializers/fund_america.rb
@@ -2,4 +2,4 @@ FundAmerica.api_key = 'n3_7NrhVfWuwptBzcKQkLzMMtTLF2Cdm'
 FundAmerica.mode = 'production'
 
 # STAGING
-##FundAmerica.api_key = 'HGW5qJtVbIM7AWLxWg-KG40DqDQpV-iS'
+#FundAmerica.api_key = 'HGW5qJtVbIM7AWLxWg-KG40DqDQpV-iS'
