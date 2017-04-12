7fc0ac9a69cc7be4a31bdff8facf972812f96957
alexander <alexandr.larionov88@gmail.com>
2017-04-12 15:27:58 -0700

    yahoo

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -39,7 +39,7 @@ class OmniauthCallbacksController < ApplicationController
   def contacts_callback
     @contacts = request.env['omnicontacts.contacts']
     @user = request.env['omnicontacts.user']
-    puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
+    #puts "List of contacts of #{@user[:name]} obtained from #{params[:importer]}:"
     @contacts.each do |contact|
       puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
     end
