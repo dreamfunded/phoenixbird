17a6b327fb888c1f78445a5ffea41658b9df1011
alexander <alexandr.larionov88@gmail.com>
2017-04-25 17:22:59 -0700

    company questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405223755) do
+ActiveRecord::Schema.define(version: 20170425225944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -788,6 +788,12 @@ ActiveRecord::Schema.define(version: 20170405223755) do
     t.string   "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "questions", force: true do |t|
+    t.text    "answer"
+    t.integer "company_id"
+    t.text    "question"
   end
 
   create_table "risks", force: true do |t|
