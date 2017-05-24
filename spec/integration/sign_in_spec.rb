019cc43e730d2a3cd541eb60df4e64a6dfe5b4bb
alexander <alexandr.larionov88@gmail.com>
2017-05-23 18:03:21 -0700

    sign up test

--- spec/integration/sign_in_spec.rb
+++ spec/integration/sign_in_spec.rb
@@ -7,7 +7,12 @@ feature 'sign in as a user' do
         fill_in 'user_email', with: 'person@email.com'
         fill_in 'user_first_name', with: 'Alexandr'
         fill_in 'user_last_name', with: 'Smith'
+        fill_in 'user_password', with: 'password'
+        fill_in 'user_password_confirmation', with: 'password'
+        check('box1')
+        check('box4')
         click_button 'Sign Up'
+        expect(page).to have_content('Alexandr')
     end
 
 end
