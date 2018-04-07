237175531877ed033fd0fd67116946ba4ab55967
alexander <alexandr.larionov88@gmail.com>
2018-04-07 13:29:24 -0700

    assets

---
+++ public/assets/signup-6bae0252898ce3730eb4f89eb20dd608aa4b7656e0aa5f58cbb6d8063d7edb30.js
@@ -0,0 +1,11 @@
+    function onOff() {
+    console.log('running')
+    var boxeschecked = 0;
+    for (var i = 1; i<= 5; i++) {
+        if(document.getElementById("box" + i).checked == true){
+        	boxeschecked ++;
+        }
+    }
+    if (boxeschecked == 0) {document.getElementById("submit-button").disabled= true;}
+    if (boxeschecked != 0) {document.getElementById("submit-button").disabled= false;}
+}
