cb1888d082e614974cc144793eb7a785c0046862
alexander <alexandr.larionov88@gmail.com>
2020-03-30 16:22:50 -0700

    fix devise error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    #before_action :configure_permitted_parameters, if: :devise_controller?
     helper_method :authority
     before_action :logout_user_session
 
